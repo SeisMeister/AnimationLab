4ee431676b39ae333af09d2ff8be341606f0f36f
Cesar Fernandez <CesarFernandez2@live.com>
2024-06-04 09:22:09 -0600

    Initial Commit

---
+++ Animation-Lab/Animation_LabApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animation_LabApp.swift
+//  Animation-Lab
+//
+//  Created by Cesar Fernandez on 6/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Animation_LabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
